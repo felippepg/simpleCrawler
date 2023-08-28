929eed609a4739a9532eefb2b3a00e87d6c54a88
Felippe <felippepg.goncalves@outlook.com.br>
2023-08-28 19:29:12 -0300

    Commit inicial

---
+++ src/test/java/br/com/felippe/simpleCrawler/SimpleCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felippe.simpleCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
